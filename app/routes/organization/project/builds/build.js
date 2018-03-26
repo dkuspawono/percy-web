784184a5fb4eebfa360769cfea075e9aab0aa430
LC <lrc3233@gmail.com>
2018-03-26 16:45:28 -0700

    clean up

--- app/routes/organization/project/builds/build.js
+++ app/routes/organization/project/builds/build.js
@@ -5,4 +5,18 @@ export default Route.extend(AuthenticatedRouteMixin, {
   model(params) {
     return this.store.findRecord('build', params.build_id);
   },
+
+  actions: {
+    createReview(action, build, snapshots) {
+      const review = this.get('store').createRecord('review', {
+        build: build,
+        snapshots: snapshots,
+      });
+      return review.save().then(() => {
+        const build = this.modelFor(this.routeName);
+        build.get('snapshots').reload();
+        build.reload();
+      });
+    },
+  },
 });
