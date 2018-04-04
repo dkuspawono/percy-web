e70089ae8fe140454c933447c08841d8f31bcea0
LC <lrc3233@gmail.com>
2018-04-04 15:18:46 -0700

    add browsers to build-info-tropdown

--- app/models/browser.js
+++ app/models/browser.js
@@ -1,6 +1,9 @@
 import DS from 'ember-data';
+import {alias} from '@ember/object/computed';
 
 export default DS.Model.extend({
   builds: DS.hasMany('build', {inverse: null}),
   browserFamily: DS.belongsTo('browserFamily', {async: false, inverse: null}),
+  version: DS.attr(),
+  name: alias('browserFamily.name'),
 });
