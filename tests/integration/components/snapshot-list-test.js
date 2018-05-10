788e00a8c49954b6aadfc362f1fa36b36b3436d3
Michael Padon <maprules1000@gmail.com>
2018-05-10 16:30:14 -0500

    Use short snapshot comparisons for many comparison test
    Reduce size of comparison images to 100 instead of 500 to reduce total
    page height from 19.8k px to 8k and not run into any browser rendering
    limitations.

--- tests/integration/components/snapshot-list-test.js
+++ tests/integration/components/snapshot-list-test.js
@@ -25,11 +25,16 @@ describe('Integration: SnapshotList', function() {
     const numSnapshots = 10;
 
     beforeEach(function() {
+      this.timeout(6000);
       const stub = sinon.stub();
       const build = make('build', 'finished');
       const browser = make('browser');
 
-      const snapshotsChanged = makeList('snapshot', numSnapshots, 'withComparisons', {build});
+      const snapshotsChanged = makeList('snapshot', numSnapshots, {build});
+      snapshotsChanged.forEach(snapshot => {
+        snapshot.set('comparisons', [make('comparison', 'short')]);
+      });
+
       this.set('snapshotsChanged', snapshotsChanged);
 
       this.setProperties({
