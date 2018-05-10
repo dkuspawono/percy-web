c1a43297656698f8706d47a4c6228c66ee24f67c
Michael Padon <maprules1000@gmail.com>
2018-05-10 10:36:36 -0500

    Add configurable data test selector to loading page component

--- app/components/loading-page.js
+++ app/components/loading-page.js
@@ -2,4 +2,6 @@ import Component from '@ember/component';
 
 export default Component.extend({
   classNames: ['LoadingPage'],
+  attributeBindings: ['dataTestId:data-test-id'],
+  dataTestId: null,
 });
