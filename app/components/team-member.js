58f4009a95800ad866bc32b4a35e5bc6ccbf9bd2
LC <lrc3233@gmail.com>
2018-05-21 14:16:11 -0700

    use new import syntax

--- app/components/team-member.js
+++ app/components/team-member.js
@@ -1,12 +1,12 @@
-import Ember from 'ember';
 import Component from '@ember/component';
 import {computed} from '@ember/object';
 import seededRandom from 'percy-web/lib/random';
+import {htmlSafe} from '@ember/string';
 
 export default Component.extend({
   tagName: '',
 
   setGridItemOrder: computed(function() {
-    return Ember.String.htmlSafe('order: ' + Math.floor(seededRandom() * 100 + 1));
+    return htmlSafe('order: ' + Math.floor(seededRandom() * 100 + 1));
   }),
 });
